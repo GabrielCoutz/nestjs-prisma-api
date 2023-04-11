7e18fce451b974ef701f76aeb3914e88dfbb3cc9
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-10 22:12:58 -0300

    feat(user-default-select): now any user request return his products, if have it

--- src/repository/selects/user-default-select.ts
+++ src/repository/selects/user-default-select.ts
@@ -3,4 +3,13 @@ export const userDefaultSelect = {
   name: true,
   email: true,
   created_at: true,
+  products: {
+    select: {
+      id: true,
+      created_at: true,
+      description: true,
+      name: true,
+      price: true,
+    },
+  },
 };
